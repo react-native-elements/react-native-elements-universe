acfb76f46625ada1ffe7f32ed1d52cc0a132cc8a
Pranshu Chittora <pranshuchittora17@gmail.com>
2021-04-18 16:30:56 +0530

    feat: Initial version of RNEU

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Native Elements Universe
---
+++ src/Example/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Example = () => {
+  return <Text>It Works</Text>;
+};
+
+export default Example;
